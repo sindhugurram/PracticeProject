10724773d69e30eaa94feac902d8dd54f0873a9a
LAPTOP-Q1TRO4KA\user <swetha.java88@gmail.com>
2017-01-18 22:50:13 -0500

    'second_commit'

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var http=require('http');
+var fs=require('fs');
 var he=require('./creatingModule.js');
 
 console.log(he.sayHello());
+
+var read_string=fs.readFileSync('tes.txt','utf8');
+console.log(read_string);
